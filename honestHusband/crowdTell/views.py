fd8b23799e1744acdb4aa9619651b951dc427d2c
Stephen Goeddel <sgoeddel247@gmail.com>
2015-08-28 22:19:37 -0400

    some half cocked shit.

--- honestHusband/crowdTell/views.py
+++ honestHusband/crowdTell/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import FormView, CreateView
+from .models import *
+
+
+class PictureCreateView(CreateView):
+    model = Picture
+    fields = ['image']
+    template_name = 'crowdTell/picture_create_view.html'
